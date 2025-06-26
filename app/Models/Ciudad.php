5356aa0f5b782429bda567a5169c1bbc86488b2b
Cracker <164807873+FBcrackero@users.noreply.github.com>
2025-06-25 19:20:11 -0500

    Update Ciudad.php

--- app/Models/Ciudad.php
+++ app/Models/Ciudad.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ciudad extends Model
 {
-    protected $table = 'ciudad'; // Nombre exacto de la tabla en tu base de datos
-    public $timestamps = false;  // Si tu tabla no tiene created_at/updated_at
+    protected $table = 'ciudad'; 
+    public $timestamps = false;  
 }
